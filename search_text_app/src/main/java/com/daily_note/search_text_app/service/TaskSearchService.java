13008a1d8ae191d55cf43ab19f01d4b8402bf4c5
Ankit Srivastava <44730520+ankitsrivastava07@users.noreply.github.com>
2026-09-21 16:30:12 +0530

    Search text code fixes

--- search_text_app/src/main/java/com/daily_note/search_text_app/service/TaskSearchService.java
+++ search_text_app/src/main/java/com/daily_note/search_text_app/service/TaskSearchService.java
@@ -48,6 +48,57 @@ public class TaskSearchService {
                     .withPageable(pageable)
                     .build();
 
+        }
+     package com.daily_note.search_text_app.service;
+
+import com.daily_note.search_text_app.entity.TaskSearchDocument;
+import org.springframework.data.domain.*;
+import org.springframework.data.elasticsearch.client.elc.NativeQuery;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.SearchHit;
+import org.springframework.data.elasticsearch.core.SearchHits;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskSearchService {
+
+    private final ElasticsearchOperations elasticsearchOperations;
+
+    public TaskSearchService(ElasticsearchOperations elasticsearchOperations) {
+        this.elasticsearchOperations = elasticsearchOperations;
+    }
+
+    public Page<TaskSearchDocument> getAllTasks(
+            String userId,
+            int page,
+            int size,
+            String search) {
+
+        Pageable pageable = PageRequest.of(
+                page,
+                size,
+                Sort.by(
+                        Sort.Direction.DESC,
+                        "createdAt"
+                )
+        );
+
+        NativeQuery query;
+
+        if (search == null || search.isBlank()) {
+
+            query = NativeQuery.builder()
+                    .withQuery(q -> q
+                            .term(t -> t
+                                    .field("userId")
+                                    .value(userId)
+                            )
+                    )
+                    .withPageable(pageable)
+                    .build();
+
         } else {
 
             String searchValue = search.trim();
@@ -94,5 +145,9 @@ public class TaskSearchService {
                 searchHits.getTotalHits()
         );
     }
+
+}   
+
+    
 
 }
